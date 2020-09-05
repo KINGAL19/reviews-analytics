2b29e61a85cdb6081c438ef337aa7a149c325ad1
KINGAL <kingal171314@gmail.com>
2020-09-05 12:48:58 +0800

    add filter

--- read1.py
+++ read1.py
@@ -4,10 +4,25 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:
+		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('----------------------------------')
-print(data[1])
+print('檔案讀取完了,總共有', len(data), '筆資料')
+# print(data[0])
+# print('----------------------------------')
+# print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均長度為', sum_len/ len(data))
+	# print(d)
+	# print(len(d))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
 
